640a098591bf52b221b48c1302262e9d77b58264
arafat-rahman01 <aabrarbin851@gmail.com>
2025-11-12 22:56:42 +0600

    greedy

---
+++ div4ProblemSolving/YourName.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;cin>>n;
+        string s;string t;
+        cin>>s>>t;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
