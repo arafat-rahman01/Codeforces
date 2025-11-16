54468916da31bbcca9a6d8aa4a4549ff680bfe05
arafat-rahman01 <aabrarbin851@gmail.com>
2025-11-16 21:35:46 +0600

    Accepted

---
+++ div2ProblemSolving/sameDiffA.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        char last=s[n-1];
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(s[i] != last)ans++;
+        }
+        cout<<ans<<endl;
+    }
+}
