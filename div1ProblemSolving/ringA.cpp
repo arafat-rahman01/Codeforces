72157c42502d62333ea14e5fadd56e88a7711fd1
arafat-rahman01 <aabrarbin851@gmail.com>
2025-11-16 21:34:36 +0600

    !

---
+++ div1ProblemSolving/ringA.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t; 
+    cin>>t;
+    while(t--){
+        int n; 
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        sort(arr,arr+n);
+        int c=0;
+        for(int i=0;i<n-1;i++){
+            c+=max(arr[i],arr[i+1]);
+        }
+        cout<<c<<endl;
+    }
+}
