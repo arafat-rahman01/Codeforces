fa41975ada9ca03358a076e14d99a336f6d61c65
arafat-rahman01 <aabrarbin851@gmail.com>
2025-11-17 22:50:58 +0600

    Accepted

--- div2ProblemSolving/ForbiddenIntegerA.cpp
+++ div2ProblemSolving/ForbiddenIntegerA.cpp
@@ -1,29 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int t;cin>>t;
-    while(t--){
-        int n,k,x;cin>>n>>k>>x;
-        if(x!=1){
-            cout<<"YES"<<endl;
-            int s=0;
-            cout<<n<<endl;
-            for(int i=0;i<n;i++){
-                cout<<1<<" ";
-            }
-            cout<<endl;
+    int t;
+    cin >> t;
+    while (t--){
+        long long n, k, x;
+        cin >> n >> k >> x;
+        
+        if (x != 1){
+            cout << "YES" << endl;
+            cout << n << endl;
+            for (int i = 1; i <= n; i++)
+                cout << 1 << " ";
+            cout << endl;
         }
-        else if(n%2==0 && x%2!=0){
-            cout<<"YES"<<endl;
-            int s=0;
-            cout<<n/2<<endl;
-            for(int i=0;i<n/2;i++){
-                cout<<2<<" ";
+        else{
+            if (k == 1 || (k == 2 && n % 2 == 1))
+                cout << "NO" << endl;
+            else{
+                cout << "YES" << endl;
+                if (n % 2 == 0){
+                    cout << n / 2 << endl;
+                    for (int i = 1; i <= n / 2; i++)
+                        cout << 2 << " ";
+                    cout << endl;
+                }
+                else{
+                    cout << (n - 3) / 2 + 1 << endl;
+                    for (int i = 1; i <= (n - 3) / 2; i++)
+                        cout << 2 << " ";
+                    cout << 3 << endl;
+                }
             }
-            cout<<endl;
-        }
-        else{ 
-            cout<<"NO"<<endl;
         }
     }
 }
