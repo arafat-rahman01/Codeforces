a219398d7ee011bbb1ea282d19626367ef7099ec
arafat-rahman01 <aabrarbin851@gmail.com>
2026-04-13 15:06:18 +0600

    sum-n-1(Math+Greedy)

---
+++ div3ProblemSolving/BthethrdSide.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n],s=0;
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+            s+=arr[i];
+        }
+        if(n==1) cout<<arr[0]<<endl;
+        else cout<<s-n-1<<endl;
+    }
+}
